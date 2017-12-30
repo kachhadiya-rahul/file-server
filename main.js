d59c0e7a83a681b067197dc07f37dfd38d0178ac
rahul-999 <rahul.kachhadiya99917@gmail.com>
2017-12-30 12:01:07 +0530

    minor code dependency eliminated or resolved

--- main.js
+++ main.js
@@ -1,120 +1,114 @@
-
+//required modules
 var express = require('express')
-  , app = module.exports = express();
-var fs= require('fs');
-var url = require('url');
-var path = require('path');
-var formalizer = require('./lib/formalizer.js');
+  , app = module.exports = express()
+var fs= require('fs')
+var url = require('url')
+var path = require('path')
+var formalizer = require('./lib/formalizer.js')
+var os = require('os')
+var bodyparser = require('body-parser')
+var async = require('async')
+
+app.use('/static',express.static('images'))
+app.use(bodyparser.urlencoded({extended:false})) //for some reason now express does not ship bodyparser inbuilt
 
 
-//this geneate the directory listing if the target url is for directory
-app.get('/*//', function(req, res){
-      //console.log(home);
-      var home = process.argv[2] || "/" ;
-      var data = formalizer.addHeader(req.hostname);
-      var pos=0;
-      var files=[],folders=[];
-      var urlParts = url.parse(req.url,true);
-      var urlPath = urlParts.pathname;
-      fs.readdir(home + urlPath.replace(/%20/g,' '), function(err, items) {
+app.get('/*',function(req,res){
+  fs.ReadStream("./test/home.html").pipe(res)
+})
+
+app.post('/*',function(req,res){
+  //platform check
+  var platform = os.platform();
+  if(platform=="linux"){
+      var home = process.argv[2] || "/home/" ;
+  }else if (platform=="win32") {
+    var home = process.argv[2] || "C:/" ;
+  }
+
+  //file or folder is requested
+  var fof = req.body.fof
+  if(fof == "file"){
+
+    var urlParts = url.parse(req.url,true);
+    var urlPath = urlParts.pathname;
+    var file = req.params.file
+      , path = home+urlPath.replace(/%20/g,' ');
+    res.download(path);
+
+  }else if(fof == "folder"){
+    var data = formalizer.addHeader(req.hostname)
+    var pos=0
+    var files=[],folders=[];
+    var urlParts = url.parse(req.url,true);
+    var urlPath = urlParts.pathname;
+    //collecting elements of the folder
+    fs.readdir(home + urlPath.replace(/%20/g,' '), function(err, items) {
+      async.waterfall([function(callback){
         var func = function(j){
-          if(items[i].indexOf("WindowsApps")==-1 && items[i].indexOf("RECYCLE")==-1 && items[i].indexOf("System")==-1 && items[i].indexOf("Config") == -1)
+          //if(items[i].indexOf("WindowsApps")==-1 && items[i].indexOf("RECYCLE")==-1 && items[i].indexOf("System")==-1 && items[i].indexOf("Config") == -1)
           fs.stat(home+ urlPath.replace(/%20/g,' ') + items[i].replace(/%20/g,' '), function (err, stats) {
 
             if (err) {
-              console.log(err);
-              process.exit(1);
+              console.log(items[j]+"has some access right issue...");
+              console.log(err)
             }
             else {
-            	//console.log(stats);
               if(stats.isDirectory()){
                 //console.log(items[j] + " is  directory");
                 folders.push(items[j].replace(/%20/g,' '));
-
               }
               else if(stats.isFile()){
-                //console.log(items[j] + " is  file ");
                 files.push(items[j].replace(/%20/g,' '));
               }
               else {
                 console.log(items[j] + " is not a file or directory");
               }
-
-
             }
           });
-      		if(j>=items.length-1 || items.length == 0 ){
-	            setTimeout(function(){
-	            	folders.sort();
-	            	files.sort();
-	            	for(var index=0;index<folders.length;index++){
-	            		data += formalizer.addFolder(folders[index],pos%4);
-	            		pos++;
-	            	}
-	            	for(var index=0;index<files.length;index++){
-	            		data += formalizer.addFile(files[index],pos%4);
-	            		pos++;
-	            	}
-	            	data +="<div></body></html>";
-	              res.send(data);
-	            },200);
-	        }
-        }
-
-        if(err){
-          console.log(err);
         }
         if(items.length == 0){
         	data +="The directory is empty...</body></html>"
-            res.send(data);
-        }
-        for (var i=0; i<items.length; i++) {
-          func(i);
-        }
+          res.send(data);
+        }else{
+          for (var i=0; i<items.length; i++) {
+            func(i);
+          }
+          setTimeout(function(){
+            callback(null)
+          },200)
 
-      });
-});
+        }
+        //console.log("hello")
+      },function(callback){
 
-//serves file ti client of sny kind
-app.get('/*\.*/', function(req, res, next){
-  var home = process.argv[2] || "F:" ;
-  var urlParts = url.parse(req.url,true);
-  var urlPath = urlParts.pathname;
-  var file = req.params.file
-    , path = home+urlPath.replace(/%20/g,' ');
-    //console.log(urlPath);
-  if(urlPath.endsWith("folder2.png")){
-    fs.readFile('./images/icons/folder2.png', function (err, data) {
-    if (err) throw err;
-      res.write(data);
-      res.end()
-    });
-  }else if(urlPath.endsWith("download.jpg")){
-    fs.readFile('./images/icons/download.jpg', function (err, data) {
-    if (err) throw err;
-      res.write(data);
-      res.end()
-    });
-  }else{
-    res.download(path);
-    //console.log("file requested...");
+        folders.sort();
+      	files.sort();
+        //console.log(files+folders)
+      	for(var index=0;index<folders.length;index++){
+      		data += formalizer.addFolder(folders[index],pos%4);
+          pos++;
+      	}
+      	for(var index=0;index<files.length;index++){
+      		data += formalizer.addFile(files[index],pos%4);
+          pos++;
+      	}
+      	data +="<div></body></html>";
+        //console.log(data)
+        //console.log("world")
+        callback(null)
+      },function(){
+        res.send(data)
+        //console.log("data sent")
+      }],function(err,success){
+        if(err) console.log("something went wrong")
+        else console.log(done)
+      })
+      //console.log("next to waterfall")
+    })
   }
 
+})
 
-});
-
-
-app.use(function(err, req, res, next){
-
-  if (404 == err.status) {
-    res.statusCode = 404;
-    res.send('Cant find that file, sorry!');
-  } else {
-    next(err);
-  }
-});
-
-if (!module.parent) {
-  app.listen(8000);
-  console.log('Express started on port %d', 8000);
-}
+app.listen(8000)
+console.log("Server is at : 8000")
